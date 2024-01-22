c9e2eda18b86770222950520c0dc08b223d375f0
Michael <80859948+bridgemami@users.noreply.github.com>
2024-01-22 14:53:55 -0800

    completed the answer

--- script.js
+++ script.js
@@ -1,58 +1,93 @@
 const questionEl = document.getElementById("question")
 const answerEl = document.body
-/* Totally Not Another FizzBuzz 
+/*  Emojify!
 
-Scrimba CEO Per Borgen wants you to write a program to grant special bonuses to all his employees based on their employee ID numbers! 
+Popular services like Slack and Github allow for emoji shortscodes, meaning 
+they will detect when a word in a sentence begins and ends with a colon (:)
+and automatically replace that word with an emoji. 
 
-Scrimba has 100 employees and their employee ID numbers range from 1 - 100. If the employee's ID number is: 
+These shortcodes allow users to add an emoji to their messages by typing a 
+code rather than searching for an emoji from a list. 
 
-Divisible by 3 - Vacation! 
-Divisible by 5 - $100,000 bonus! 
-Divisible by both 3 and 5 - JACKPOT! 1 Million and a Yacht!
-Not divisible by 3 or 5 - :(
-    
-Write a program to loop through all the ID numbers and print their prize. 
-Your function's output should look something like this: 
+For example, typing :smile: will replace that text with 😊 
 
-1 - :(
-2 - :(
-3 - Vacation! 
-4 - :(
-5 - $100,000 bonus!
+*/
 
-Hint: Remainder operator, modulo 
- */
-
-questionEl.innerHTML= `Scrimba has 100 employees and their employee ID numbers range from 1 - 100. If the employee's ID number is: <br /><br />
-Divisible by 3 - Vacation! <br /><br />
-Divisible by 5 - $100,000 bonus! <br /><br />
-Divisible by both 3 and 5 - JACKPOT! 1 Million and a Yacht! <br /><br />
-Not divisible by 3 or 5 - :( <br /><br />
-    
-Write a program to loop through all the ID numbers and print their prize. `
-// Test your function
-function awardBonuses(){
-    // 1) loop through 1-100
-    // 2) which number is divisiable by 3, 5, 3 & 5, or neither
-    for( let i =1; i <= 100; i++) {
-        if(i % 15 === 0) {
-            answerEl.innerHTML += `<p><strong>${i}: JACKPOT! 1 Million and a Yacht!</strong></p>`
-            console.log(`${i}: JACKPOT! 1 Million and a Yacht!`)
-        }
-        else if(i % 5 === 0) {
-            answerEl.innerHTML += `<p>${i}: $100,000 bonus!</p>`
-            console.log(`${i}: $100,000 bonus!`)
-        }
-        else if ( i % 3 === 0) {
-            answerEl.innerHTML += `<p>${i}: Vacation!</p>`
-            console.log(`${i}: Vacation!`)
- 
-        }
-        else {
-            answerEl.innerHTML += `<p>${i}: 😞</p>`
-            console.log(`${i}: 😞`)
-        }
-    }
+const emojis = {
+    "smile": "😊",
+    "angry": "😠",
+    "party": "🎉",
+    "heart": "💜",
+    "cat":   "🐱",
+    "dog":   "🐕"
 }
 
-awardBonuses();
+questionEl.innerHTML= `<ol>
+<li> Write a function that checks if a lowercase word starts and 
+ends with a colon. If it does, remove the colons and
+look up the word in the emoji object. If the word is in the 
+emojis object, return the corresponding emoji.
+If it isn't, return the original word.</li><br />
+<li>Write a function to find any emoji shortcodes in a phrase.
+Your function should map over each word in the phrase, emojify any word
+that begins and ends with a colon, then return the emojified phrase. 
+Feel free to use your emojify function from the previous exercise!</li>
+</ol>`
+/* 1. Write a function that checks if a lowercase word starts and 
+ends with a colon. If it does, remove the colons and
+look up the word in the emoji object. If the word is in the 
+emojis object, return the corresponding emoji.
+If it isn't, return the original word.
+
+Example input: ":party:"
+Example output: 🎉
+
+Example input: ":flower:"
+Example output: "flower"
+
+Example input: "elephant"
+Example output: "elephant"
+*/ 
+
+function emojifyWord(word){
+    // 1) check to see if it starts and end with :
+    // 2) reove the :
+    // 3) check if the word matches with one the object's property 
+    if(word.startsWith(":") && word.endsWith(":")) {
+        const removeHypen = word.slice(1,-1)
+       return emojis[removeHypen] ? emojis[removeHypen]  : removeHypen
+    }
+    else {
+        return word
+    }
+}
+/* 2. Write a function to find any emoji shortcodes in a phrase.
+Your function should map over each word in the phrase, emojify any word
+that begins and ends with a colon, then return the emojified phrase. 
+Feel free to use your emojify function from the previous exercise!
+
+Example input: "I :heart: my :cat:"
+Example output: "I 💜 my 🐱"
+
+Example input: "I :heart: my elephant"
+Example output: "I 💜 my elephant"
+*/ 
+
+function emojifyPhrase(phrase){
+    // 1) use split method on the sentece
+    // 2) map the new array with emojiyWord function
+    // 3) join the the array with a space
+    const splitting = phrase.split(" ")
+    const rephrase = splitting.map(word => emojifyWord(word)).join(' ')
+    answerEl.innerHTML += `<p>${rephrase}</p>`
+    return rephrase
+}
+
+
+
+console.log(emojifyWord(":heart:"));
+console.log(emojifyWord(":flower:"));
+console.log(emojifyWord("elephant"));
+
+console.log(emojifyPhrase("I :heart: my :cat:"));
+console.log(emojifyPhrase("I :heart: my :elephant:"));
