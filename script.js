f80effb400b082f5a16ea63b7a38fc1cc11b9fd4
Michael <80859948+bridgemami@users.noreply.github.com>
2024-01-22 13:53:54 -0800

    answered the question and added a title in index.html

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
 const questionEl = document.getElementById("question")
 const answerEl = document.body
-/* toTitleCase
-Write a function that will capitalize every word in a sentence.  
+/* Totally Not Another FizzBuzz 
 
-Example Input: "everything, everywhere, all at once"
-Example Output: "Everything, Everywhere, All At Once"
-*/
+Scrimba CEO Per Borgen wants you to write a program to grant special bonuses to all his employees based on their employee ID numbers! 
 
-/* 
-First, write a function that takes in one word and 
-capitalizes the first letter of that word.
+Scrimba has 100 employees and their employee ID numbers range from 1 - 100. If the employee's ID number is: 
 
-Example Input: "scrimba"
-Example Output: "Scrimba"
+Divisible by 3 - Vacation! 
+Divisible by 5 - $100,000 bonus! 
+Divisible by both 3 and 5 - JACKPOT! 1 Million and a Yacht!
+Not divisible by 3 or 5 - :(
+    
+Write a program to loop through all the ID numbers and print their prize. 
+Your function's output should look something like this: 
 
-Hint: Trying using slice() and .toUpperCase()
-*/
+1 - :(
+2 - :(
+3 - Vacation! 
+4 - :(
+5 - $100,000 bonus!
 
-questionEl.textContent= `Write a function that will capitalize every word in a sentence. `
+Hint: Remainder operator, modulo 
+ */
+
+questionEl.innerHTML= `Scrimba has 100 employees and their employee ID numbers range from 1 - 100. If the employee's ID number is: <br /><br />
+Divisible by 3 - Vacation! <br /><br />
+Divisible by 5 - $100,000 bonus! <br /><br />
+Divisible by both 3 and 5 - JACKPOT! 1 Million and a Yacht! <br /><br />
+Not divisible by 3 or 5 - :( <br /><br />
+    
+Write a program to loop through all the ID numbers and print their prize. `
 // Test your function
-function capitalizeWord(word){
-    // 1) I need to capitalize the first letter and capitalize it
-    // 2) rejoing the capitalize letter
-    const newStr = word.slice(0,1).toUpperCase()+ word.slice(1)
-    answerEl.innerHTML += `<p>For a word: ${newStr}</p>`
-    return newStr
+function awardBonuses(){
+    // 1) loop through 1-100
+    // 2) which number is divisiable by 3, 5, 3 & 5, or neither
+    for( let i =1; i <= 100; i++) {
+        if(i % 15 === 0) {
+            answerEl.innerHTML += `<p><strong>${i}: JACKPOT! 1 Million and a Yacht!</strong></p>`
+            console.log(`${i}: JACKPOT! 1 Million and a Yacht!`)
+        }
+        else if(i % 5 === 0) {
+            answerEl.innerHTML += `<p>${i}: $100,000 bonus!</p>`
+            console.log(`${i}: $100,000 bonus!`)
+        }
+        else if ( i % 3 === 0) {
+            answerEl.innerHTML += `<p>${i}: Vacation!</p>`
+            console.log(`${i}: Vacation!`)
+ 
+        }
+        else {
+            answerEl.innerHTML += `<p>${i}: 😞</p>`
+            console.log(`${i}: 😞`)
+        }
+    }
 }
 
-/* 
-Now write a function that capitalizes every word in a sentence. 
-How can you reuse the function you just wrote? 
-*/ 
-
-function toTitleCase(str){
-    // 1) I need to loop through the sentence
-    // 2) Each word I need to capital the first letter
-    // 3) Then recreate the senetence with the new change
-    let splittingTheString=  str.split(' ')
-    const newStr = splittingTheString.map(word => { 
-          return capitalizeWord(word)
-    }).join(' ')
-    answerEl.innerHTML += `<p>For a sentence: ${newStr}</p>`
-return newStr
-}
-
-// Test your functions
-console.log(capitalizeWord("pumpkin"));
-console.log(toTitleCase("pumpkin pranced purposefully across the pond"));
+awardBonuses();
