17f96d7ba5bf54111b47d8d460b9e1713b34ef81
Michael <80859948+bridgemami@users.noreply.github.com>
2024-01-22 12:47:04 -0800

    answered the question

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
 const questionEl = document.getElementById("question")
 const answerEl = document.body
-/* Whispering function 
-Write a function `whisper` that takes in a sentence 
-and returns a new sentence in all lowercase letters with
-"shh..." at the beginning. 
+/* Alternating Caps 
+ Write a function that takes in a string of letters
+ and returns a sentence in which every other letter is capitalized.
 
-The function should also remove an exclamation point
-at the end of the sentence, if there is one. 
-
-Example 
-input: "The KITTENS are SLEEPING!"
-output: "shh... the kittens are sleeping"
-
-Hint: endsWith and slice
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
 */
-questionEl.textContent= `Write a function "whisper" that takes in a sentence 
-and returns a new sentence in all lowercase letters with
-"shh..." at the beginning.`
 
+questionEl.textContent= `Write a function that takes in a string of letters
+and returns a sentence in which every other letter is capitalized.`
 // Test your function
-function whisper(str) {
-    // 1) concat the shh
-    // 2) convert the string to lower case
-    // 3) first three steps make into one variable
-    // 4) check if the the string ends with ! 
-    const translate = 'shhh... ' + str.toLowerCase()
-    if(str.endsWith('!')){
-        const withPointAtEnd = translate.slice(0, -1)
-        answerEl.innerHTML += `<p>${withPointAtEnd}</p>`
-        return withPointAtEnd
+function altCaps(str){
+    // 1) create a new variable for the new string 
+    // 2) loop over the string
+    // 3) find the even index or zero position and convert it to a capitalize letter
+    // 4) for the odd number index convert it to lower case
+    // 5) make sure the new string joins together
+    let newStr = ``
+    for(let i = 0; i < str.length; i++) {
+        if(i % 2 ===0) {
+            newStr += str[i].toUpperCase()
+        }
+        else {
+            newStr += str[i].toLowerCase()
+        }
     }
-    else {
-        answerEl.innerHTML += `<p>${translate}</p>`
-        return  translate
-    }
+    answerEl.innerHTML +=`<p>${newStr}</p>`
+    return newStr;
 }
 
-console.log(whisper("PLEASE STOP SHOUTING."));
-console.log(whisper("MA'AM, this is a Wendy's!"));
+console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
