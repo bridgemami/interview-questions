02f1f0eab36de566f02d941a84477ff62d3b3609
Michael <80859948+bridgemami@users.noreply.github.com>
2024-01-23 10:39:11 -0800

    answered the question

--- script.js
+++ script.js
@@ -1,59 +1,37 @@
 const questionEl = document.getElementById("question")
 const answerEl = document.body
-/* We Come in Peace!   
-We've received what (we assume) is a message of peace and brotherhood from 
-an alien planet. They almost got it right, but the messages are 
-backward. Write functions to reverse the backward messages so we can 
-read what they have to say! 
-*/ 
+/*  
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.
 
-const title = ":htraE no od ot ffutS";
-const messages = [
-            "maerc eci yrT",
-            "rewoT leffiE tisiV",
-            "noom eht ot snamuh etacoleR",
-            "egrahc ni stac tuP", 
-        ]
+Example input: "motorbike"
+Example output: false
+
+Example input: "rotator" 
+Example output: true
+*/
 
 
-questionEl.innerHTML += `<p>We Come in Peace! <br />   
-We've received what (we assume) is a message of peace and brotherhood from 
-an alien planet. They almost got it right, but the messages are 
-backward.<br />
-Write functions to reverse the backward messages so we can 
-read what they have to say!</p>`
+questionEl.textContent += `Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.`
 
-/* Step 1: Reverse a string
-Write a function that takes in a string and returns the reverse 
-of that string. An interviewer may want to check if you know your
-string methods, or may want to know if you can reverse a string manually. 
-Practice both ways! 
 
-Example input: !htrae ot emocleW
-Example output: Welcome to earth!
-*/  
+// Solution 1: for loop 
+function isPalindrome(str){
+    // 1) split the string
+    // 2) reverse the new array then join it
+    // 3) check to see if the word is equal to the orginal word
+    const reversingStr = str.split('').reverse().join('')
+    return answerEl.innerHTML+= reversingStr === str ? `<p>${str}: ${true}</p>` : `<p>${str}: ${false}</p>`
 
-function reverseString(str){
-    // 1) switch string to array with split method
-    // 2) use the reverse method
-    // 3) use the join mehod to make it a string
-    const reverseString = str.split('').reverse().join('');
-    answerEl.innerHTML += `<p>${reverseString}</p>`
-    return reverseString
 }
 
-/*
-Step 2: Now we'll reverse all strings in an array. Write a function that takes in
-an array of strings and returns a new array with all strings reversed.
-
-You can use reuse your reverseString() function, use string methods, or 
-reverse the strings manually. 
-*/ 
-
-function reverseStringsInArray(arr){
-    // 4) map the array with the reverseString function
-    return arr.map(a => reverseString(a)).join(' ')
-}
-
-console.log(reverseString(title));
-console.log(reverseStringsInArray(messages));
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
