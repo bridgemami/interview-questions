55f2b5a59979da6c84b0c233f4bc65ebdf37b6b3
Michael <80859948+bridgemami@users.noreply.github.com>
2024-01-23 11:03:05 -0800

    added another example for the function

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
 const questionEl = document.getElementById("question")
 const answerEl = document.body
 /*  
-Palindromes are words that are the same forward or backward. For example, 
-the words "noon" and "kayak" are a palindromes.
- 
-Write a function to check if a lowercased string of letters is a palindrome. 
-If the word is palindrome, return true. If it isn't, return false.
+Grandpa's hand isn't as steady as it used to be. You finally convinced him
+to start using a password manager, but he accidentally typed and saved his
+password with a bunch of extra characters. Help him recover his password by 
+removing all the duplicate characters. 
 
-Example input: "motorbike"
-Example output: false
+Your function should take in a string of characters and return a
+string with the duplicate characters removed. Assume that your input
+is lowercased with only letters and numbers.  
 
-Example input: "rotator" 
-Example output: true
-*/
+Example input: "aabbccb1212"
+Example output: "abc12"
+*/ 
 
 
-questionEl.textContent += `Write a function to check if a lowercased string of letters is a palindrome. 
-If the word is palindrome, return true. If it isn't, return false.`
+questionEl.innerHTML += `Your function should take in a string of characters and return a
+string with the duplicate characters removed. Assume that your input
+is lowercased with only letters and numbers.  
+<br /><br />
+Example input: "aabbccb1212"
+Example output: "abc12"`
 
 
 // Solution 1: for loop 
-function isPalindrome(str){
-    // 1) split the string
-    // 2) reverse the new array then join it
-    // 3) check to see if the word is equal to the orginal word
-    const reversingStr = str.split('').reverse().join('')
-    return answerEl.innerHTML+= reversingStr === str ? `<p>${str}: ${true}</p>` : `<p>${str}: ${false}</p>`
-
+const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23"; 
+ 
+function removeDupeChars(chars){
+    // 1) convert the string to an array
+    // 2) create a new array without the dulicates
+    // 3) loop over the array
+    // 4) while looping compare what exist in the new string
+    // 5) join the new array into a string
+    const trimPassword = chars.split('')
+    let cleanPassword =[]
+    for( let i =0; i< trimPassword.length; ++i) {
+        if(cleanPassword.includes(trimPassword[i])) {
+            null
+        }
+        else {
+           cleanPassword.push(trimPassword[i])
+        }
+    }
+    answerEl.innerHTML += `<p>${cleanPassword.join('')}</p>`
+    return cleanPassword.join('')
 }
 
-// Test your function
-console.log(isPalindrome("abba"));
-console.log(isPalindrome("civic"));
-console.log(isPalindrome("octopus"));
-console.log(isPalindrome("pumpkins"));
-console.log(isPalindrome("madam"));
+console.log(removeDupeChars(password));
+console.log(removeDupeChars('aabbccb1212'));
