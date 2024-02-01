4126098c8184eaf2c42aec701c6c3515b992b459
Michael <80859948+bridgemami@users.noreply.github.com>
2024-02-01 15:58:55 -0800

    completed the answer

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
 const answerEl = document.body
 const questionEl = document.getElementById("question")
-/* Find All Unique Tags 
+/* Welcome Aboard Scrimba Airlines 
 
-As a software dev at ScrimFlix, you're working on a feature 
-to let users browse TV shows by tag. The first step is to collect all 
-the tags from our data into a new array. Then we'll need 
-to filter out the duplicate tags. 
+Our Scrimba Airlines in-flight entertainment package 
+includes a variety of podcasts. We need to add a feature that suggests
+podcasts to our patrons based on whether a flight is short or long. 
 
-Write a function that takes in the media data and returns
-a flat array of unique tags.
+Your sort function should take two arguments: the podcast data and
+flight length. If the flight is 60 minutes or less, sort the podcast list 
+from shortest to longest. If it's anything else, sort from longest
+to shortest. 
 
-Expected output: 
-["supernatural", "horror", "drama",
-"fantasy", "reality", "home improvement", "comedy", "sci-fi", "adventure"]
+Your function shouldn't return anything. Instead log a numbered list 
+of the title and duration of 
+each podcast to the console, like this:
 
-*/ 
+1. Crime Fan, 150 minutes
+2. Mythical Creatures, 99 minutes
+3. Crime Crime Crime, 70 minutes
+4. Coding Corner, 55 minutes
+5. Scrimba Podcast, 50 minutes
+6. Something about Witches, 35 minutes
 
-questionEl.innerHTML += `Write a function that takes in the media data and returns
-a flat array of unique tags. 
+*/
+
+questionEl.innerHTML += `Your sort function should take two arguments: the podcast data and
+flight length. If the flight is 60 minutes or less, sort the podcast list 
+from shortest to longest. If it's anything else, sort from longest
+to shortest.  
 <br /><br />
-Expected output: 
-["supernatural", "horror", "drama",
-"fantasy", "reality", "home improvement", "comedy", "sci-fi", "adventure"]
+Expected output:<br />
+1. Crime Fan, 150 minutes<br />
+2. Mythical Creatures, 99 minutes<br />
+3. Crime Crime Crime, 70 minutes<br />
+4. Coding Corner, 55 minutes<br />
+5. Scrimba Podcast, 50 minutes<br />
+6. Something about Witches, 35 minutes<br />
 `
 
-import mediaData from "./data.js";
+import podcasts from "./data.js";
 
-function getUniqueTags(data){
-   const flatening = data.map(db => {
-    return db.tags
-   }).flat()
-   const checking = {}
-   const uniqueTags = flatening.filter(db => {
-    if(!checking[db]){
-        checking[db]= true
-        return true
+function sortByDuration(data, flightLength){
+  const sortArray = data.sort((a,b) => {
+    if(flightLength > 60){
+        return b.duration - a.duration
     }
-    return false
-   })
-   answerEl.innerHTML+= `<p>${uniqueTags.join(', ')}</p>`
-   return uniqueTags
+    else{
+        return a.duration - b.duration
+    }
+  })
+  const order = sortArray.forEach((a, i) => {
+    answerEl.innerHTML += `<p>${i + 1}. ${a.title}, ${a.duration} minutes</p>`
+   return `${i + 1}. ${a.title}, ${a.duration} minutes`
+})
+return order
 }
 
-console.log(getUniqueTags(mediaData));
+sortByDuration(podcasts, 60)
+sortByDuration(podcasts, 61)
