8ab9a5a41cf962f61e148cf8c03c1e3917c7992a
Michael <80859948+bridgemami@users.noreply.github.com>
2024-02-05 09:24:22 -0800

    completed question

--- data.js
+++ data.js
@@ -1,27 +1,59 @@
 export default [
-    {
-       post: "A lot of you have asked about my morning skin care routine...",
-       likes: 348
-    },
-    {
-       post: "New Year, new me. #blessed",
-       likes: 102
-    },
-    {
-       post: "Sunday by the pool 🌴 #vibes #mood #hotdoglegs",
-       likes: 544
-    },
-    {
-       post: "Here are my top 5 #notsponsored unbiased dry shampoo recs...",
-       likes: 745
-    },
-    {
-       post: "Last day in Hawaii! The tans will fade but the memories will last forever #holidays #foreverhawaii #blessed #followme",
-       likes: 412
-    },
-    {
-       post: "Leg day #summerbody2022 #gymshark #beachworkout",
-       likes: 275
-    },
+   {
+       id: 1,
+       title: "Scrimba Podcast", 
+       duration: 50, 
+       tags: ["education", "jobs", "technology"], 
+       hosts: ["Alex Booker"], 
+       rating: 10,
+       genre: "education",
+       paid: false
+   },
+   {
+       id: 2,
+       title: "Crime Fan", 
+       duration: 150, 
+       tags: ["crime", "entertainment", "mature"], 
+       hosts: ["Bob Smith", "Camilla Lambert"], 
+       genre: "true crime",
+       rating: 5, 
+       paid: true
+   },
+   {
+       id: 3,
+       title: "Mythical Creatures", 
+       duration: 99, 
+       tags: ["entertainment", "general", "unicorns"], 
+       hosts: ["Esmerelda Shelley", "Duke Dukington", "Felix the Cat"], 
+       genre: "fantasy",
+       rating: 8,
+       paid: true
+   },
+   {
+       title: "Crime Crime Crime", 
+       duration: 70, 
+       tags: ["crime", "entertainment", "mature"], 
+       hosts: ["Jessica Jones", "Humphrey Bogart", "Inspector Gadget"], 
+       genre: "true crime",
+       rating: 6, 
+       paid: true
+   },
+   {
+       title: "Something about Witches", 
+       duration: 35, 
+       tags: ["fantasy", "entertainment"], 
+       hosts: ["Frewin Wyrm", "Evanora Highmore"], 
+       genre: "fantasy",
+       rating: 8,
+       paid: false
+   },
+   {
+       title: "Coding Corner", 
+       duration: 55, 
+       tags: ["education", "jobs", "technology"], 
+       hosts: ["Treasure Porth", "Guil Hernandez", "Tom Chant"], 
+       genre: "education",
+       rating: 9,
+       paid: false
+   },
 ]
-  
--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 const answerEl = document.body
 const questionEl = document.getElementById("question")
-/* Popularity Contest 
+/* Night at the Scrimbies 
 
-Iggy the Influencer and Toby the Tiktoker are dying to know
-who's more popular on social media. 
+It's time for the Scrimbies, a prestigious award show for podcast hosts.
+We need to assemble a list of podcast hosts so we can start handing out awards. 
 
-Toby's TikToks get an average of 400 likes. On average, how many
-likes do Iggy's Instagram posts get? 
+Write a function that takes in the podcast data and
+returns a flat array of podcast hosts. There are quite a few ways to approach
+this, but try solving the problem using reduce(). 
 
-In data.js you'll find a list of Iggy's recent posts. 
-Use reduce() to write a function that returns the average number of likes.
-To find the average, add up the total number of likes, then divide
-by the total number of posts.
-*/
+Once you have a flat array of hosts, write a second function to randomly assign each host a prize
+from the awards array. 
+
+Example output: ["🏆 Alex Booker", "⭐ Bob Smith", "💎 Camilla Lambert" ...] 
+
+*/ 
 
 questionEl.innerHTML += `Use reduce() to write a function that returns the average number of likes.
 `
 
-import postData from "./data.js";
+import podcasts from "./data.js";
 
-function calcAverageLikes(data){
-  const sum = data.reduce((a,b) => {
-        a += b.likes
-        return a
-  },0) 
-  return (sum / data.length).toFixed(2)
-} 
+const awards = ["🏆", "⭐", "💎", "🥇", "👑"];
+
+function getHosts(data){
+  let flat = data.reduce((a,b) => {
+         return a.concat(b.hosts)
+ }, []
+ )
+ return flat
+}
+
+function assignAwards(data){
+ const hosts = getHosts(data)
+ const randomReward = () => Math.floor(Math.random() * awards.length)
+const award = hosts.map(a => `${awards[randomReward()]} ${a}`)
+answerEl.innerHTML += `${award.join(', ')}`
+return award
+}
 
 
-console.log(calcAverageLikes(postData))
+console.log(getHosts(podcasts));
+console.log(assignAwards(podcasts));
